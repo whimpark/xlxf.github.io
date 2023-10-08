b7d1d4578aa3d2c08c86bb6b07860b73a2dff363
xlxwhy <1509868568@qq.com>
2023-10-08 20:30:21 +0800

    base

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -27,32 +27,33 @@ module.exports = {
     themeConfig: {
         displayAllHeaders: true, // 默认值：false
         sidebar: [
-            {
-              title: '鹿坊文章',
-              path: '/article/content',
-              collapsable: false, // 可选的, 默认值是 true,
-              sidebarDepth: 0,    // 可选的, 默认值是 1
-              children: [ 
-                {
-                  title: '百度',
-                  path: '/article/baidu/content',
-                  collapsable: false, // 可选的, 默认值是 true,
-                  sidebarDepth: 0,    // 可选的, 默认值是 1
-                  children: [ 
-                    '/article/baidu/997-231008-baidu-ad', 
-                    '/article/baidu/997-231008-baidu-bjh',  
-                  ],
-                }, 
-                '/article/google/content',  
-                '/article/weixin/content',  
-              ],
-            },
+            // {
+            //   title: '鹿坊文章',
+            //   path: '/article/content',
+            //   collapsable: false, // 可选的, 默认值是 true,
+            //   sidebarDepth: 0,    // 可选的, 默认值是 1
+            //   children: [ 
+            //     {
+            //       title: '百度',
+            //       path: '/article/baidu/content',
+            //       collapsable: false, // 可选的, 默认值是 true,
+            //       sidebarDepth: 0,    // 可选的, 默认值是 1
+            //       children: [ 
+            //         '/article/baidu/997-231008-baidu-ad', 
+            //         '/article/baidu/997-231008-baidu-bjh',  
+            //       ],
+            //     }, 
+            //     '/article/google/content',  
+            //     '/article/weixin/content',  
+            //   ],
+            // },
             {
               title: '鹿坊笔记',
               path: '/note/index',
               collapsable: false, // 可选的, 默认值是 true,
               sidebarDepth: 0,    // 可选的, 默认值是 1
               children: [
+                "/note/997-231008-stock",
                 "/note/997-231008-idea",
                 '/note/997-231008-vuepress', 
               ],
